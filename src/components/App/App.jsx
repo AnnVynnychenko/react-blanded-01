72fcceeffd504a837299a253abd7fa223704af23
AnnVynnychenkoa <avinnichenko93@gmail.com>
2023-07-23 11:20:38 +0300

    task-01

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,14 +8,24 @@ import {
   CryptoHistory,
 } from 'components';
 
+import article from 'data/article.json';
+
 export const App = () => {
   return (
     <Section>
       <Container>
-        {/* <Heading marginBottom="50px" textAlign="center"> */}
+        <Heading marginBottom="50px" textAlign="center">
           Task 1
         </Heading>
-        <BlogCard />
+        <BlogCard
+          poster={article.poster}
+          tag={article.tag}
+          title={article.title}
+          description={article.description}
+          userName={article.name}
+          avatar={article.avatar}
+          postedAt={article.postedAt}
+        />
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 2
