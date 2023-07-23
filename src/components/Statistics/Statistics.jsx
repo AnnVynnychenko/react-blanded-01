a68835718a429d5a73aff9c81421a612e66740dc
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-07-23 11:29:13 +0200

    addtask 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,20 @@
+import PropTypes from 'prop-types';
 import { StatisticItem } from 'components';
 import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+export const Statistics = ({title, stats}) => {
   return (
     <>
-      <div>StatisticsList</div>
+    {title && <StatisticTitle>{title}</StatisticTitle>}
+<StatisticsList>{stats.map(stat => <StatisticItem key={stat.id} title={stat.title} total={stat.total}/>)}</StatisticsList>
     </>
   );
 };
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.shape({id:PropTypes.string.isRequired})).isRequired
+}
